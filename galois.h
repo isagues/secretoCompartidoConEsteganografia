53344c82ba4ccc5b69d36525a190fe29687d913d
fpannunzio <fpannunzio@itba.edu.ar>
2021-05-10 00:29:35 -0300

    Added the galois operation functionalities

---
+++ galois.h
@@ -0,0 +1,12 @@
+#ifndef __GALOIS__LIB__
+#define __GALOIS__LIB__
+
+#include <stdint.h>
+
+//sources: https://en.wikipedia.org/wiki/Finite_field_arithmetic
+
+uint8_t gadd(uint8_t a, uint8_t b);
+
+uint8_t gmul(uint8_t a, uint8_t b);
+
+#endif
