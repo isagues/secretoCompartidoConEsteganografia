373af639c7d8b3a3bc45b483dca674606221ce7b
Tobias <tbrandy@itba.edu.ar>
2021-05-11 21:28:55 -0300

    Agrego Lagrange Interpolation y typedef galois2_8_t

--- src/galois.c
+++ src/galois.c
@@ -2,7 +2,11 @@
 
 // https://en.wikipedia.org/wiki/Finite_field_arithmetic#Program_examples
 /* Add two numbers in the GF(2^8) finite field */
-uint8_t gadd(uint8_t a, uint8_t b) {
+galois2_8_t gadd(galois2_8_t a, galois2_8_t b) {
+	return a ^ b;
+}
+
+galois2_8_t gsub(galois2_8_t a, galois2_8_t b) {
 	return a ^ b;
 }
 
@@ -11,8 +15,8 @@ uint8_t gadd(uint8_t a, uint8_t b) {
  * using the Russian Peasant Multiplication algorithm
  * (the other way being to do carry-less multiplication followed by a modular reduction)
  */
-uint8_t gmul(uint8_t a, uint8_t b) {
-	uint8_t p = 0; /* the product of the multiplication */
+galois2_8_t gmul(galois2_8_t a, galois2_8_t b) {
+	galois2_8_t p = 0; /* the product of the multiplication */
 	while (a && b) {
             if (b & 1) /* if b is odd, then add the corresponding a to p (final product = sum of all a's corresponding to odd b's) */
                 p ^= a; /* since we're in GF(2^m), addition is an XOR */
@@ -26,21 +30,70 @@ uint8_t gmul(uint8_t a, uint8_t b) {
 	return p;
 }
 
-uint8_t gpow(uint8_t base, uint8_t exp) {
+galois2_8_t gdiv(galois2_8_t a, galois2_8_t b) {
+    // TODO(tobi)
+	return 1;
+}
+
+galois2_8_t gpow(galois2_8_t base, galois2_8_t exp) {
     int result = base;
-    for (uint8_t i = 0; i < exp; i++)   
-    {
+    for (uint8_t i = 0; i < exp; i++) {
         result = gmul(result, base);
     }
     return result;
 }
 
-uint8_t evaluate_pol(uint8_t * coeficients, uint8_t size, uint8_t x_value) {
+galois2_8_t evaluate_pol(galois2_8_t coeficients[], uint8_t n, galois2_8_t x_value) {
     uint8_t result = coeficients[0];
     
-    for (uint8_t i = 1; i < size; i++)
-    {
+    for (uint8_t i = 1; i < n; i++) {
         result = gadd(result, gmul(coeficients[i], gpow(x_value, i)));
     }
     return result;
+}
+
+/**
+ * Calcula la produtoria intermedia del termino i del coeficiente r.
+ * 
+ * @param x Puntos a interpolar
+ * @param n Cantidad de puntos a interpolar
+ * @param r Indice del coeficiente
+ * @param i Indice del termino a calcular
+ */
+static galois2_8_t lagrange_interpolation_coefficient_intermediate_product(galois2_8_t x[], uint8_t n, uint8_t r, uint8_t i) {
+    galois2_8_t ret = MUL_IDENTITY;
+
+    for(uint8_t q = 0; q < n - r; q++) {
+        // ret *= x[q] / (x[i] - x[q]);
+        ret = gmul(ret, gdiv(x[q], gsub(x[i], x[q])));
+    }
+
+    return ret;
+}
+
+void lagrange_interpolation(galois2_8_t x[], galois2_8_t y[], galois2_8_t p[], uint8_t n) {
+
+    // r = coefficient_idx
+    for(uint8_t r = 0; r < n; r++) {
+        galois2_8_t coefficient = 0;
+        galois2_8_t y_prime = y[0];
+
+        for(uint8_t i = 0; i < n - r; i++) {
+            if(i > 0) {
+                // y_prime = (y[i] + p[0]) / x[i];
+                y_prime = gdiv(gsub(y[i], p[0]), x[i]);
+            }
+
+            // coefficient += y_prime * lagrange_interpolation_coefficient_intermediate_product(x, n, r, p);
+            coefficient = gadd(coefficient, gmul(y_prime, lagrange_interpolation_coefficient_intermediate_product(x, n, r, i)));
+        }
+
+        // No es necesario pues en G(2^8) un numero es su propio inverso
+        /* if(n - r % 2 == 0) {
+        *     coefficient = -coefficient;
+        *  }
+        */
+
+       p[r] = coefficient;
+    }
 }
--- src/galois.h
+++ src/galois.h
@@ -6,29 +6,34 @@
 
 #define GENERATOR_POL 0x163
 
+#define MUL_IDENTITY 1
+#define SUM_IDENTITY 0
+
+typedef uint8_t galois2_8_t;
+
 //sources: https://en.wikipedia.org/wiki/Finite_field_arithmetic
 
-uint8_t gadd(uint8_t a, uint8_t b);
+galois2_8_t gadd(galois2_8_t a, galois2_8_t b);
 
-uint8_t gmul(uint8_t a, uint8_t b);
+galois2_8_t gmul(galois2_8_t a, galois2_8_t b);
 
-uint8_t gpow(uint8_t base, uint8_t exp);
+galois2_8_t gpow(galois2_8_t base, galois2_8_t exp);
 
-uint8_t evaluate_pol(uint8_t * coeficients, uint8_t size, uint8_t x_value);
+galois2_8_t evaluate_pol(galois2_8_t coeficients[], uint8_t n, galois2_8_t x_value);
 
 /**
- * Custom Implementation of Lagrange Interpolation \n
+ * Custom Implementation of Lagrange Interpolation in Galois Field G(2^8)\n
  * 
  * Calculates the Lagrange Polynomial P(x) such that P(x[i]) = y[i] \n
  * 
  * (https://en.wikipedia.org/wiki/Lagrange_polynomial)
  * 
- * @param x     Points to interpolate
- * @param y     Values to interpolate
+ * @param x     Points to interpolate in G(2^8)
+ * @param y     Values to interpolate in G(2^8)
  * @param p     P(x) coefficients are saved in ascending order here
  * @param n     Number of points and values to interpolate, and P(x)'s degree. 
  *              All arrays must be of at least this size.
  */
-void lagrange_interpolation(uint8_t x[], uint8_t y[], uint8_t p[], uint8_t n);
+void lagrange_interpolation(galois2_8_t x[], galois2_8_t y[], galois2_8_t p[], uint8_t n);
 
 #endif
